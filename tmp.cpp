a2ec2841744ab3ac3a452cb14f51fc370b2c2294
Nastya <hurmuzz23@gmail.com>
2021-10-21 18:25:29 +0300

    doesn't work

--- tmp.cpp
+++ tmp.cpp
@@ -4,20 +4,27 @@
 
 #include <vector>
 #include "iostream"
+#include "vector.hpp"
 
 int main(void)
 {
-	std::vector<int> f;
+//	std::vector<int> f;
+//
+//	f.push_back(2);
+//	f.push_back(1);
+//
+//	std::vector<int>::iterator it = f.begin();
+//	std::vector<int>::iterator it2 = it + 1;
+//
+//	if (it > it2)
+//		std::cout << "true" << std::endl;
+//	else
+//		std::cout << "false" << std::endl;
+//	return (0);
 
-	f.push_back(2);
-	f.push_back(1);
+	ft::vector<int> v_i;
+	ft::vector<int> v_i_2(4, 6);
+//	ft::vector<int>::iterator it;
 
-	std::vector<int>::iterator it = f.begin();
-	std::vector<int>::iterator it2 = it + 1;
 
-	if (it > it2)
-		std::cout << "true" << std::endl;
-	else
-		std::cout << "false" << std::endl;
-	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -10,14 +10,16 @@
 namespace ft {
 
 	template < class T, class Alloc = std::allocator<T> >
-	class vector<T>
+	class vector
 	{
 	private:
-
+		T *_array;
+		size_t _size;
+		size_t _mem_size;
 
 	public:
-		typedef typename T value_type;
-		typedef typename Alloc allocator_type;
+		typedef T value_type;
+		typedef Alloc allocator_type;
 		typedef typename allocator_type::reference reference;
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
@@ -26,124 +28,224 @@ namespace ft {
 //		typedef typename reverse_iterator
 //		typedef typename const_reverse_iterator
 //		typedef typename difference_type
-//		typedef typename size_type
+		typedef size_t size_type; // ?
 
 
-		template<class T, class Distance = ptrdiff_t,
-				class Pointer = T*, class Reference = T&>
-		class iterator<T> // TODO: random access iterator
-		{
-		private:
-			T* _ptr;
-		public:
+//		template<class Distance = ptrdiff_t,
+//				class Pointer = T*, class Reference = T&>
+//		class iterator // TODO: random access iterator
+//		{
+//		private:
+//			T* _ptr;
+//		public:
+//
+////			typedef T value_type;
+////			typedef Distance difference_type;
+////			typedef Pointer pointer;
+////			typedef Reference reference;
+//
+//			typedef T value_type;
+//			typedef ptrdiff_t difference_type;
+//			typedef T* pointer;
+//			typedef T& reference;
+//
+//			iterator() : _ptr(nullptr) {}
+////			iterator(T *data) : _ptr(data) {}
+//			iterator(const interator<T> & src)
+//			{
+//				*this = src;
+//			}
+//			virtual ~iterator() {}
+//
+//			iterator &operator=(const iterator & src)
+//			{
+//				if (this == &src)
+//					return (*this);
+//
+//				_ptr = src->_ptr;
+//				return (*this);
+//			}
+//
+//			bool operator==(const iterator<T> & src) const
+//			{
+//				return (this->_ptr == src->_ptr);
+//			}
+//			bool operator!=(const iterator<T> & src) const
+//			{
+//				return (this->_ptr != src->_ptr);
+//			}
+//			const T &operator*()
+//			{
+//				return (*_ptr);
+//			}
+//			const T &operator*() const
+//			{
+//				return (*_ptr);
+//			}
+//			T *operator->()
+//			{
+//				return (_ptr);
+//			}
+//			T *operator->() const
+//			{
+//				return (_ptr);
+//			}
+//			iterator<T> &operator++()
+//			{
+//				_ptr++;
+//				return (*this);
+//			}
+//			iterator<T> &operator++(int)
+//			{
+//				iterator<T> tmp(*this);
+//
+//				++(*this);
+//				return (tmp);
+//			}
+////			const T & operator*++(int)
+////			{
+////				T tmp = *_ptr;
+////
+////				*_ptr++;
+////				return (tmp);
+////			} //?
+//			iterator<T> &operator--()
+//			{
+//				_ptr--;
+//				return (*this);
+//			}
+//			iterator<T> &operator--(int)
+//			{
+//				iterator<T> tmp(*this);
+//
+//				--(*this);
+//				return (tmp);
+//			}
+////			const T &operator*--(int)
+////			{
+////				T tmp = *_ptr;
+////
+////				*_ptr--;
+////				return (tmp);
+////			} //?
+//			iterator<T> &operator+(int n) const
+//			{
+//				return (_ptr + n * difference_type);
+//			}
+//			iterator<T> &operator-(int n) const
+//			{
+//				return (_ptr - n * difference_type);
+//			}
+//			bool operator<(const iterator<T> & src) const
+//			{
+//				return (this->_ptr < src->_ptr);
+//			}
+//			bool operator>(const iterator<T> & src) const
+//			{
+//				return (this->_ptr > src->_ptr);
+//			}
+//			bool operator<=(const iterator<T> & src) const
+//			{
+//				return (*this < src || *this == src);
+//			}
+//			bool operator>=(const iterator<T> & src) const
+//			{
+//				return (*this > src || *this == src);
+//			}
+//			iterator<T> &operator+=(int n)
+//			{
+//				_ptr += n * difference_type ;
+//				return (_ptr);
+//			}
+//			iterator<T> &operator-=(int n)
+//			{
+//				_ptr -= n * difference_type;
+//				return (_ptr);
+//			}
+//			const T &operator[](size_t n)
+//			{
+//				return (*(_ptr + n * difference_type));
+//			}
+//			const T &operator[](size_t n) const
+//			{
+//				return (*(_ptr + n * difference_type));
+//			}
+//		};
 
-			typedef typename T value_type;
-			typedef typename Distance difference_type;
-			typedef typename Pointer pointer;
-			typedef typename Reference reference;
-
-			iterator() : _ptr(nullptr) {}
-//			iterator(T *data) : _ptr(data) {}
-			iterator(const interator<T> & src)
-			{
-				*this = src;
-			}
-			virtual ~iterator() {}
-
-			iterator & operator=(const iterator & src)
-			{
-				if (this == &src)
-					return (*this);
-
-				_ptr = src->_ptr;
-				return (*this);
-			}
-
-			bool operator==(const iterator<T> & src) const
-			{
-				return (this->_ptr == src->_ptr);
-			}
-			bool operator!=(const iterator<T> & src) const
-			{
-				return (this->_ptr != src->_ptr);
-			}
-			T &operator*()
-			{
-				return (*_ptr);
-			}
-			T &operator*() const
-			{
-				return (*_ptr);
-			}
-			T *operator->()
-			{
-				return (_ptr);
-			}
-			T *operator->() const
-			{
-				return (_ptr);
-			}
-			iterator &operator++()
-			{
-				_ptr++;
-				return (*this);
-			}
-			iterator &operator++(int)
-			{
-				iterator<T> tmp(*this);
-
-				++(*this);
-				return (tmp);
-			}
-			iterator &operator--()
-			{
-				_ptr--;
-				return (*this);
-			}
-			iterator &operator--(int)
-			{
-				iterator<T> tmp(*this);
-
-				--(*this);
-				return (tmp);
-			}
-			bool operator<(const iterator<T> & src) const
-			{
-				return (this->_ptr < src->_ptr);
-			}
-			bool operator>(const iterator<T> & src) const
-			{
-				return (this->_ptr > src->_ptr);
-			}
-			bool operator<=(const iterator<T> & src) const
-			{
-				return (*this < src || *this == src);
-			}
-			bool operator>=(const iterator<T> & src) const
-			{
-				return (*this > src || *this == src);
-			}
-
-		};
-
-		typedef typename vector<T>::iterator iterator;
-		typedef typename vector<const T>::iterator const_iterator;
+//		typedef typename vector<T, Alloc>::iterator iterator;
+//		typedef typename vector<const T, Alloc>::iterator const_iterator;
 
 
 		// constructor
-		explicit vector(const allocator_type& alloc = allocator_type()) {}
+		explicit vector(const allocator_type & alloc = allocator_type()) :
+		_size(0), _mem_size(2)
+		{
+			_array = alloc.allocate(_mem_size);
+		}
 		explicit vector(size_type n, const value_type& val = value_type(),
-						 const allocator_type& alloc = allocator_type()) {}
+						 const allocator_type& alloc = allocator_type()) :
+						 _size(n), _mem_size(n * 2)
+		{
+			size_t i;
+
+			_array = alloc.allocate(_mem_size);
+			for (i = 0; i < n; i++)
+				alloc.construct(&_array[i], val);
+		}
 		template <class InputIterator>
 		vector(InputIterator first, InputIterator last,
-				const allocator_type& alloc = allocator_type()) {}
-		vector(const vector<T> & x) {}
+				const allocator_type & alloc = allocator_type())
+		{
+			if (first > last)
+			{
+				_size = 0;
+				_mem_size = 2;
+				_array = alloc.allocate(_mem_size);
+			}
+			else
+			{
+				InputIterator tmp = first;
+				size_t	count = 0;
+
+				for( ; tmp != last; tmp++)
+					count++;
+				_size = count;
+				_mem_size = count * 2;
+				_array = alloc.allocate(_mem_size);
+				count = 0;
+				for ( ; first != last; first++)
+					alloc.construct(&_array[count++], *first);
+			}
+		}
+		vector(const vector<T, Alloc> & x)
+		{
+			*this = x;
+		}
 
 		// destructor
-		virtual ~vector() {}
+		virtual ~vector()
+		{
+			allocator_type & alloc = allocator_type();
+
+			for (size_t i = 0; i < _size; i++)
+				alloc.destroy(&_array[i]);
+			alloc.deallocate(_array, _mem_size);
+		}
 
 		// operator = overload
-		vector<T> & operator=(const vector<T> & src) {}
+		vector<T, Alloc> & operator=(const vector<T, Alloc> & src)
+		{
+			allocator_type& alloc = allocator_type();
+
+			if (this == &src)
+				return (*this);
+
+			_size = src._size;
+			_mem_size = src._mem_size;
+			_array = alloc.allocate(_mem_size);
+			for (size_t i = 0; i < _size; i++)
+				alloc.construct(&_array[i], src._array[i]);
+		}
 
 
 
